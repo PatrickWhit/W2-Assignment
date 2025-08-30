8b02df271433152860e40041ad9e398e4e2353ce
PatrickWhit <ptrckwhitney@gmail.com>
2025-08-30 14:01:57 -0500

    Updated Character class

--- Character.cs
+++ Character.cs
@@ -2,6 +2,25 @@
 {
     public class Character
     {
+        public string Name { get; set; }
+        public string CharClass { get; set; }
+        public string Lvl { get; set; }
+        public string Hp { get; set; }
+        public List<string> Equipment { get; set; }
 
+
+        public Character()
+        {
+
+        }
+
+        public Character(string name, string charClass, string lvl, string hp, List<string> equipment)
+        {
+            Name = name;
+            CharClass = charClass;
+            Lvl = lvl;
+            Hp = hp;
+            Equipment = equipment;
+        }
     }
 }
